61739f3c3f672507ce35e8f9fd5ca94e464511e7
hdl740 <137336559@qq.com>
2017-10-14 02:04:25 +0800

    fsdfsdfsdfs

--- app/src/controller/DevController.java
+++ app/src/controller/DevController.java
@@ -12,7 +12,7 @@ import service.DevUserService;
 import entity.DevUser;
 
 @Controller
-@RequestMapping("dev123")
+@RequestMapping("dev")
 public class DevController {
 	@Autowired
 	private DevUserService devUserService;
